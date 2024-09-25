437b7ed99149ae2f6d8d1d7576d185f0b56c8c76
Vaibhavi Kahar <122308685+vaibhavikahar11@users.noreply.github.com>
2024-09-25 19:34:17 +0530

    made changes in app.js updated some of the props

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar' 
+
+
+const Home = () => {
+  return (
+    <div className='bg-black w-full h-screen'>
+     <Navbar />   
+    </div>
+  )
+}
+
+export default Home
